92863bb2ed921eedc0dd6066b0a32411b358329f
sasgod <tjrgus515@naver.com>
2021-12-03 16:44:43 +0900

    testtt

--- test/test.js
+++ test/test.js
@@ -8,15 +8,4 @@ test('로그인', async t => {
     // Test code
     await t
     .click('.tools_list > li')
-    .typeText('#mem_id', 'tjrgus515')
-    .typeText('#pwd', 'qatest1!')
-    .click('#divG_H_LOGIN_LAYER > a.login_btn.mt_20');
-});
-test('로그아웃', async t => {
-    await t
-    .click('.asdtasdasdools_list > li')
-    .typeText('#mem_id', 'tjrgus515')
-    .typeText('#pwd', 'qatest1!')
-    .click('#divG_H_LOGIN_LAYER > a.login_btn.mt_20')
-    .click('#divG_H_LOGON > li:nth-child(1) > span > a');
 });
